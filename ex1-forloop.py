72b14b47fe5046ff230f9a7b4e375d474d5d6255
Tharun-2 <ctharun_ece2004d5@mgit.ac.in>
2022-07-13 18:23:14 +0530

    2nd commit: modified ex1 and ex2 python files

---
+++ ex1-forloop.py
@@ -0,0 +1,8 @@
+n= int(input('enter the value of n: '))
+def count_to_n(x = n):
+    for i in range(1,x+1):
+        print(i,end='')
+    print()
+
+print('Going to count to ()...'.format(n))
+count_to_n(n)
---
+++ ex2-dfunction.py
@@ -0,0 +1,4 @@
+def double(n):
+    return 2*n 
+print("To Call this function using its name and assign the value of you")
+print("the program ran successfully")
