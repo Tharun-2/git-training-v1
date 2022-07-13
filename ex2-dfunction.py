fc8582ca77475f93b8977e118c71c2304bb5a6ca
Tharun-2 <87661483+Tharun-2@users.noreply.github.com>
2022-07-13 21:30:13 +0530

    Updated the file ex2 with the print statement
    Updated the file ex2 with the print statement for the first time from Github

--- ex2-dfunction.py
+++ ex2-dfunction.py
@@ -2,3 +2,4 @@ def double(n):
     return 2*n 
 print("To Call this function using its name and assign the value of you")
 print("the program ran successfully")
+print("Added this line from Github website for the first time") 
